0cab46e77d22611529d3c36571bc38b9c316877e
Isaac Corley <icorley94@gmail.com>
2017-04-13 12:45:29 -0500

    Add files via upload

--- rgb_image_slicing_MSE/PredictRGB.py
+++ rgb_image_slicing_MSE/PredictRGB.py
@@ -3,26 +3,39 @@ from scipy.misc import imread, imsave, imresize
 from keras.models import model_from_json
 from os.path import join
 
+
+#%% Define performance metrics
+def mse(y_true, y_pred):
+    MSE = np.mean(np.square(y_pred - y_true))   
+    return MSE
+
+def psnr(y_true, y_pred):
+    max_pixel_val = 255
+    MSE = np.mean(np.square(y_pred - y_true))
+    PSNR = (10. * np.log10(np.square(max_pixel_val) / MSE))
+    return PSNR 
+
+
 #%% Define variables
 input_size = (128, 128)
 label_size = (256, 256)
-saved_model_dir = 'rgb_image_slicing_SSIM/model'
+saved_model_dir = 'msemodel/64x64'
 model_name = ('Rmodel.json', 'Gmodel.json', 'Bmodel.json')
 saved_weights_name = ('Rmodel_weights.h5', 'Gmodel_weights.h5', 'Bmodel_weights.h5')
-output_dir = 'rgb_image_slicing_SSIM/output'
+output_dir = 'output'
 
 # Define the window size
-windowsize_r = 8
-windowsize_c = 8
+windowsize_r = 64
+windowsize_c = 64
 
 image_dir = 'lena.bmp'
 
 
-#%% Rescale images 
+#%% Rescale images
 image = imread(image_dir)
 scaled = imresize(image, input_size, 'bicubic')
 scaled = imresize(scaled, label_size, 'bicubic')
-image = imresize(image, label_size, 'bicubic')
+ref_img = (imresize(image, label_size, 'bicubic'))
 
 input_img = scaled.copy()
 output_img = np.zeros(input_img.shape)
@@ -30,7 +43,7 @@ output_img = np.zeros(input_img.shape)
 
 #%% Loop over all channels
 for i in range(3):
-    
+
     # Load model and weights separately due to error in keras for custom loss functions
     model = model_from_json(open(join(saved_model_dir, model_name[i])).read())
     model.load_weights(join(saved_model_dir, saved_weights_name[i]))
@@ -40,7 +53,7 @@ for i in range(3):
     input_chan = np.expand_dims(input_chan, axis=3)
     model_input = []
     model_output = np.zeros(input_chan.shape)
-    
+
     # Splice image channel
     for r in range(0,input_chan.shape[0] - windowsize_r + 1, windowsize_r):
     	for c in range(0,input_chan.shape[1] - windowsize_c + 1, windowsize_c):
@@ -49,8 +62,8 @@ for i in range(3):
 
     # Predict output
     output = model.predict(model_input)
-    
-    
+
+
     # Reconstruct channel from slices
     j = 0
     for r in range(0, model_output.shape[0] - windowsize_r + 1, windowsize_r):
@@ -64,6 +77,11 @@ for i in range(3):
 
 
 #%% Save images
+output_img = output_img.astype('uint8')
 imsave(join(output_dir, "scaled_image.jpg"), scaled)
 imsave(join(output_dir, "output_image.jpg"), output_img)
-imsave(join(output_dir, "reference_image.jpg"), image)
+imsave(join(output_dir, "reference_image.jpg"), ref_img)
+
+#%% 
+print('Scaled MSE:', mse(ref_img, scaled), 'PSNR:', psnr(ref_img, scaled))
+print('Output MSE:', mse(ref_img, output_img), 'PSNR:', psnr(ref_img, output_img))
