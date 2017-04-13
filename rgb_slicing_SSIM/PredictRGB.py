245776d8945ffadb84322019ac56c4fe8c735081
Isaac Corley <icorley94@gmail.com>
2017-04-12 21:47:13 -0500

    Add files via upload
    Up to date files to train and predict on all rgb channels

--- rgb_slicing_SSIM/PredictRGB.py
+++ rgb_slicing_SSIM/PredictRGB.py
@@ -2,18 +2,18 @@ import numpy as np
 from scipy.misc import imread, imsave, imresize
 from keras.models import model_from_json
 from os.path import join
-import matplotlib.pyplot as plt
 
 #%% Define variables
 input_size = (128, 128)
 label_size = (256, 256)
+saved_model_dir = 'BSDS500Resized/model'
 model_name = ('Rmodel.json', 'Gmodel.json', 'Bmodel.json')
-model_weights_name = ('Rmodel_weights.h5', 'Gmodel_weights.h5', 'Bmodel_weights.h5')
+saved_weights_name = ('Rmodel_weights.h5', 'Gmodel_weights.h5', 'Bmodel_weights.h5')
 output_dir = 'output'
 
 # Define the window size
-windowsize_r = 32
-windowsize_c = 32
+windowsize_r = 8
+windowsize_c = 8
 
 image_dir = 'a10.jpg'
 
@@ -22,50 +22,45 @@ image_dir = 'a10.jpg'
 image = imread(image_dir)
 scaled = imresize(image, input_size, 'bicubic')
 scaled = imresize(scaled, label_size, 'bicubic')
-image =imresize(image, label_size, 'bicubic')
+image = imresize(image, label_size, 'bicubic')
 
-
-#%% Load model and weights separately due to error in keras
-model = model_from_json(open(saved_model_dir).read())
-model.load_weights(saved_weights_dir)
-
-
-#%% Reshape scaled image for input to model (must be 4 dimensions with channels last)
-#input_img = np.expand_dims(scaled, axis=0)
-#input_img = np.transpose(scaled, (1, 2, 0))
-#input_img = np.expand_dims(scaled, axis=0)
 input_img = scaled.copy()
-
-#%%
-
-input_img = np.asarray(input_img)
-print(input_img.shape)
-
-#%%
-input_imgs = []
 output_img = np.zeros(input_img.shape)
 
-#%%
 
-for r in range(0,input_img.shape[0] - windowsize_r + 1, windowsize_r):
-	for c in range(0,input_img.shape[1] - windowsize_c + 1, windowsize_c):
-		input_imgs.append(input_img[r:r+windowsize_r,c:c+windowsize_c])
-input_imgs = np.asarray(input_imgs)
+#%% Loop over all channels
+for i in range(3):
+    
+    # Load model and weights separately due to error in keras for custom loss functions
+    model = model_from_json(open(join(saved_model_dir, model_name[i])).read())
+    model.load_weights(join(saved_model_dir, saved_weights_name[i]))
 
-#%%
-print(input_imgs.shape)
-tmp = input_imgs[0,:,:,:]
+    # Separate by channel
+    input_chan = input_img[:,:,i]
+    input_chan = np.expand_dims(input_chan, axis=3)
+    model_input = []
+    model_output = np.zeros(input_chan.shape)
+    
+    # Splice image channel
+    for r in range(0,input_chan.shape[0] - windowsize_r + 1, windowsize_r):
+    	for c in range(0,input_chan.shape[1] - windowsize_c + 1, windowsize_c):
+    		model_input.append(input_chan[r:r+windowsize_r,c:c+windowsize_c])
+    model_input = np.asarray(model_input)
 
-#%%
-output = model.predict(input_imgs)
+    # Predict output
+    output = model.predict(model_input)
+    
+    
+    # Reconstruct channel from slices
+    j = 0
+    for r in range(0, model_output.shape[0] - windowsize_r + 1, windowsize_r):
+    	for c in range(0,model_output.shape[1] - windowsize_c + 1, windowsize_c):
+    		model_output[r:r+windowsize_r,c:c+windowsize_c] = output[j,:,:]
+    		j += 1
 
-
-#%%
-i = 0
-for r in range(0, output_img.shape[0] - windowsize_r + 1, windowsize_r):
-	for c in range(0,output_img.shape[1] - windowsize_c + 1, windowsize_c):
-		output_img[r:r+windowsize_r,c:c+windowsize_c] = output[i,:,:,:]
-		i += 1
+    # Add reconstructed output to channel index
+    model_output = np.squeeze(model_output)
+    output_img[:,:,i] = model_output
 
 
 #%% Save images
